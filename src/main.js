96ee6ddbdfb5f3e5798324179d0c346a138cdf64
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-20 17:54:17 +0330

    coustomize color and fonts added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "@/style.css"
+import "@/assets/fonts.scss"
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      'errorRed':'hsl(0, 100%, 66%)',
+      'purpleBorder':'hsl(249, 99%, 64%) ',
+      'darkViolet':'hsl(278, 68%, 11%)',
+      'darkGray':'hsl(279, 6%, 55%)',
+      'lightGray':'hsl(270, 3%, 87%)',
+      'darkPurple':'hsl(278, 94%, 30%)'
+    }
   },
   plugins: [],
 }
