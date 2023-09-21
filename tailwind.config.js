e60a9c501331c2b18f710cb24d64633695ec7612
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-21 16:42:49 +0330

    adding font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      'errorRed':'hsl(0, 100%, 66%)',
-      'purpleBorder':'hsl(249, 99%, 64%) ',
-      'darkViolet':'hsl(278, 68%, 11%)',
-      'darkGray':'hsl(279, 6%, 55%)',
-      'lightGray':'hsl(270, 3%, 87%)',
-      'darkPurple':'hsl(278, 94%, 30%)'
+    fontFamily:{
+      'spaceBold':["'space-bold'"],
+      'spaceSemiBold':['space-semiBold'],
+      'spaceMedium':['space-medium']
+    },
+    extend: {
+      colors:{
+        'errorRed':'hsl(0, 100%, 66%)',
+        'purpleBorder':'hsl(249, 99%, 64%) ',
+        'darkViolet':'hsl(278, 68%, 11%)',
+        'darkGray':'hsl(279, 6%, 55%)',
+        'lightGray':'hsl(270, 3%, 87%)',
+        'darkPurple':'hsl(278, 94%, 30%)',
+      },
     }
   },
   plugins: [],
