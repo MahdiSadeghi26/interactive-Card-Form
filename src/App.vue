24f237c69722181b529b40a873068ac6cf96daec
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-22 14:46:00 +0330

    card positon fixed in md

--- src/App.vue
+++ src/App.vue
@@ -8,36 +8,34 @@ let sucessFlag = ref(false)
 <template>
   <!-- grid container -->
   <div class="grid lg:grid-cols-2 h-screen  ">
-    <div class="flex xl:inline ">
+    <div class="flex xl:inline md:justify-center ">
       <!-- backgrounds -->
-      <img src="/image/bg-main-desktop.png" alt="desktop backgrorund" class="lg:inline sm:hidden lg:row-span-2 w-3/5 h-screen ">
+      <img src="/image/bg-main-desktop.png" alt="desktop backgrorund"
+        class="lg:inline sm:hidden lg:row-span-2 w-3/5 h-screen ">
       <img src="/image/bg-main-mobile.png" alt="mobile background" class="lg:hidden sm:inline sm:w-full sm:h-[270px]">
       <!-- front card -->
-      <div class="absolute xl:top-[17%] lg:top-[15%] xl:left-[12%] lg:left-[7%] text-slate-50 font-spaceMedium z-20 ">
+      <div class="absolute xl:top-[17%] lg:top-[15%] xl:left-[12%] lg:left-[7%] md:top-[21%] md:left-[30%] md:w-[300px] text-slate-50 font-spaceMedium z-20 ">
         <img src="image/bg-card-front.png" alt="card front" class="xl:w-[430px] lg:w-[370px]">
-        <img src="image/card-logo.svg" alt="card logo"
-          class="absolute xl:top-[10%] xl:left-[10%] lg:top-[10%] lg:left-[10%] lg:w-[70px]">
-        <p
-          class="inline  absolute xl:bottom-[30%] xl:text-3xl  xl:left-[9%] tracking-widest lg:bottom-[29%] lg:left-[11%] lg:text-2xl">
-          9591 6489 6389 101E</p>
+        <img src="image/card-logo.svg" alt="card logo" class="absolute xl:top-[10%] md:top-5 md:left-5 xl:left-[10%] lg:top-[10%] lg:left-[10%] md:w-[60px]">
+        <p class="inline  absolute xl:bottom-[30%] md:bottom-[25%] md:text-lg md:left-10 xl:text-3xl  xl:left-[9%] tracking-widest lg:bottom-[29%] lg:left-[11%] lg:text-2xl">9591 6489 6389 101E</p>
         <!-- card holder name -->
-        <p class="absolute lg:bottom-[10%] lg:left-[10%] ">FELICA LEIRE</p>
-        <span class="absolute lg:right-[10%] lg:bottom-[10%]">
+        <p class="absolute lg:bottom-[10%] xl:left-[10%] md:left-[12%] md:bottom-[10%]  ">FELICA LEIRE</p>
+        <span class="absolute md:right-[10%] md:bottom-[10%]  ">
           <p class="inline">09</p> / <p class="inline">00</p>
         </span>
       </div>
       <!-- back cart -->
-      <div class="absolute lg:bottom-[15%]   xl:left-[17%] lg:left-[14%] xl:w-[430px] lg:w-[370px] md:w-[300px]">
+      <div class="absolute lg:bottom-[15%]   xl:left-[17%] lg:left-[14%] xl:w-[430px] lg:w-[370px] md:w-[300px] md:bottom-[69%]">
         <img src="/image/bg-card-back.png" class="" alt="">
         <p
-          class="absolute xl:top-[100px] font-spaceMedium tracking-wider xl:right-12 text-xl text-slate-50 lg:top-[85px] lg:right-10 ">
+          class="absolute xl:top-[100px] font-spaceMedium tracking-wider xl:right-12 text-xl text-slate-50 lg:top-[85px] md:right-10 md:top-[42%] md:text-base ">
           000</p>
       </div>
     </div>
     <!-- inputs -->
     <div class="flex  items-center xl:justify-center justify-center   ">
-       <!-- success section (displays if everything is valid) -->
-       <div class="flex flex-col items-center  xl:w-[400px]" v-if="sucessFlag">
+      <!-- success section (displays if everything is valid) -->
+      <div class="flex flex-col items-center  xl:w-[400px]" v-if="sucessFlag">
         <img src="/image/icon-complete.svg" alt="completed">
         <p class="mt-8 text-3xl font-spaceSemiBold tracking-widest">THANK YOU!</p>
         <p class="tracking-wide text-darkGray mt-4 font-spaceMedium">We've added your card details</p>
@@ -66,7 +64,7 @@ let sucessFlag = ref(false)
             class="w-full bg-darkViolet text-slate-50 mt-8 text-[17px] xl:p-4 lg:p-3 rounded-lg font-spaceMedium ">Confirm</button>
         </form>
       </div>
-     
+
     </div>
   </div>
 </template>
