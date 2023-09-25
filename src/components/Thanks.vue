39a4b76487358a3039d4fe56a9f03511dbbb21f0
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-25 23:57:33 +0330

    adding form state inside cmp

--- src/components/Thanks.vue
+++ src/components/Thanks.vue
@@ -1,14 +1,18 @@
 <script setup>
+import { useFormStore } from '@/stores/Form'
 
+// Inputes section flag (input fild and )
+const formStore = useFormStore()
 
 </script>
 <template>
     <!-- success section (displays if everything is valid) -->
-    <div class="flex flex-col items-center  xl:w-[400px]">
-        <img src="/image/icon-complete.svg" alt="completed">
-        <p class="mt-8 text-3xl font-spaceSemiBold tracking-widest">THANK YOU!</p>
-        <p class="tracking-wide text-darkGray mt-4 font-spaceMedium">We've added your card details</p>
-        <button class="bg-darkViolet text-slate-50 font-spaceBold mt-5 w-full rounded-md p-4">Continue</button>
-    </div>
+    <div class="flex flex-col items-center  xl:w-[400px]" v-show="formStore.isSubmited">
+            <img src="/image/icon-complete.svg" alt="completed">
+            <p class="mt-8 text-3xl font-spaceSemiBold tracking-widest">THANK YOU!</p>
+            <p class="tracking-wide text-darkGray mt-4 font-spaceMedium">We've added your card details</p>
+            <button @click="formStore.isSubmited=false"  class="bg-darkViolet text-slate-50 font-spaceBold mt-5 w-full rounded-md p-4">Continue</button>
+
+        </div>
 </template>
 
