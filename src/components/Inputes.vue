c99f253e3932168d62a2ccc85c47785da5b1a214
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-25 08:18:38 +0330

    Aadding input mask for credit card number

--- src/components/Inputes.vue
+++ src/components/Inputes.vue
@@ -1,13 +1,44 @@
 <script setup>
+import { watch, ref } from 'vue';
+import { useCardStore } from '@/stores/Card'
+
+let cardState = useCardStore()
+let maskedNumber = ref('')
+
+const formatCardNumber = (event) => {
+    let input = event.target.value;
+    // Remove any non-digit characters from the input
+    let formattedInput = input.replace(/\D/g, "");
+    let maskedInput = "";
+
+    // Apply the card number format
+    for (let i = 0; i < formattedInput.length; i++) {
+        if (i > 0 && i % 4 === 0) {
+            maskedInput += " ";
+        }
+        maskedInput += formattedInput[i];
+    }
+
+    maskedNumber.value = maskedInput;
+};
+
+// Watch for changes in the maskedNumber and trim any extra spaces
+watch(maskedNumber, (newValue) => {
+    maskedNumber.value = newValue.trim();
+    cardState.cardNumber = maskedNumber.value
+});
+
+
 
 </script>
 <template>
     <div class="flex justify-center lg:w-[350px] md:w-[350px] w-auto">
-        <form action="#" class="xl:w-[400px] lg:w-full" >
+        <form action="#" class="xl:w-full lg:w-full">
             <p class="lable">CARDHOLDER NAME</p>
-            <input type="text" placeholder="e.g. Jane Applessed" class="bigInput !mb-5">
+            <input type="text" placeholder="e.g. Jane Applessed" class="bigInput !mb-5" v-model="cardState.cardHolder">
             <p class="lable">CARD NUMBER</p>
-            <input type="text" placeholder="e.g. 1234 5678 9123 0000" class="bigInput">
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" class="bigInput " v-model="maskedNumber"
+                maxlength="19" @input="formatCardNumber">
             <div class="w-full mt-8 flex">
                 <!-- exp date -->
                 <div class="w-1/2">
